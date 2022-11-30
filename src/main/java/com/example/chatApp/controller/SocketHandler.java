fa2c9b16999d95015b1f4b67294f15c563b9f090
Ahmodiyy <ahmodolaitan03@gmail.com>
2022-11-30 08:10:04 +0100

    text based websocker

---
+++ src/main/java/com/example/chatApp/controller/SocketHandler.java
@@ -0,0 +1,41 @@
+package com.example.chatApp.controller;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws InterruptedException, IOException {
+        for(WebSocketSession webSocketSession : sessions) {
+            System.out.println("payload  "+ message.getPayload());
+            //Map value = new Gson().fromJson(message.getPayload(), Map.class);
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+
+}
---
+++ src/test/java/com/example/chatApp/messagingstompwebsocket/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatApp.messagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
